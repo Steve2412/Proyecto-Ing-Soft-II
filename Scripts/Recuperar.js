88f98693ef25eafdde78448215fb8d7bbbf60147
Steve <stefano007casa@hotmail.com>
2023-06-13 23:45:57 -0400

    Recuperar contraseña
    Funcion de recuperar contraseña añadido, envia un codigo al correo.

--- Scripts/Recuperar.js
+++ Scripts/Recuperar.js
@@ -1,4 +1,16 @@
-var Insertar_Nombre = document.querySelector("#Correo");
+var codigo = Math.random() * 1000000;
+codigo = Math.trunc(codigo)
+console.log(codigo);
+msg = "Tu codigo de recuperacion es: " + codigo;
+console.log(msg); 
+
+
+var Formulario = document.querySelector(".Formulario");
+var Insertar_Correo = document.querySelector("#Correo");
+var Insertar_Codigo = document.querySelector("#Codigo");
+var Insertar_Contra_1 = document.querySelector("#Contraseña-Nueva_1");
+var Insertar_Contra_2 = document.querySelector("#Contraseña-Nueva_2");
+
 
 var el_first = document.querySelector(".Entrada-Datos-1");
 var el_second = document.querySelector(".Entrada-Datos-2");
@@ -8,15 +20,91 @@ var boton_codigo = document.querySelector(".Boton-Codigo");
 var boton_cambiar = document.querySelector(".Boton-Cambiar");
 
 function fun_siguiente() {
-  el_first.style.display = "none";
-  el_second.style.display = "block";
-  boton_siguiente.style.display = "none";
-  boton_codigo.style.display = "block";
+  var datos = new FormData(Formulario);
+  fetch("php/verificar.php", {
+    method: "POST",
+    body: datos,
+  })
+    .then(res => res.json())
+    .then(data => {
+      console.log(data);
+      if (data === "2") {
+        alert("No existe ningun usuario en la base de datos con este correo");
+      } else if (data == "1") {
+        Email.send({
+          SecureToken : "4c3095ea-2148-4fb7-b505-43905012346e",
+          To : Insertar_Correo.value,
+          From : "fanmedichistes24@gmail.com",
+          Subject : "Codigo de recuperacion de cuenta",
+          Body : msg
+      }).then(
+        message => alert(message)
+      );
+      alert(
+        "Te has llegado un correo con un codigo, busca en la bandeja de entrada"
+        
+      )
+        el_first.style.display = "none";
+        el_second.style.display = "block";
+        boton_siguiente.style.display = "none";
+        boton_codigo.style.display = "block";;
+      }
+    });
+
 }
 
 function fun_Codigo() {
-  el_second.style.display = "none";
-  el_third.style.display = "block";
-  boton_codigo.style.display = "none";
-  boton_cambiar.style.display = "block";
-}
+
+  if(Insertar_Codigo.value==codigo){
+    alert("Codigo correcto");
+    el_second.style.display = "none";
+    el_third.style.display = "block";
+    boton_codigo.style.display = "none";
+    boton_cambiar.style.display = "block";
+  }else{
+    alert("Codigo incorrecto");
+  }
+  }
+
+  function fun_Cambio(){
+
+    if(Insertar_Contra_1 == null||Insertar_Contra_1 == ""||Insertar_Contra_2 == null||Insertar_Contra_2 == ""){
+      alert("Falta campos por rellenar")
+    }else if (Insertar_Contra_1.value.length < 8) {
+      alert("Contraseña menor a 8 caracteres");
+    } else if (Insertar_Contra_1.value.length > 16) {
+      alert("Contraseña mayor a 16 caracteres");
+    } else if (Insertar_Contra_1.value.search(/[a-z]/) < 0) {
+      alert("Debe Incluir una letra minuscula");
+    } else if (Insertar_Contra_1.value.search(/[A-Z]/) < 0) {
+      alert("Debe Incluir una letra Mayuscula");
+    } else if (Insertar_Contra_1.value.search(/[0-9]/) < 0) {
+      alert("Debe Incluir una Numero");
+    } else if (Insertar_Contra_1.value !== Insertar_Contra_2.value) {
+      alert("Las contraseñas no concuerdan");
+    }else{
+      var datos_2 = new FormData(Formulario);
+      console.log(datos_2.get("Correo"));
+      console.log(datos_2.get("Contra"));
+      fetch("php/recuperar.php", {
+        method: "POST",
+        body: datos_2,
+      })
+        .then(res => res.json())
+        .then(data => {
+          console.log(data);
+          if (data === "1") {
+            alert("Contraseña cambiada correctamente");
+            //window.location.href = "login.html";
+          } else if (data === "2") {
+            alert(
+              "Te has registrado correctamente en el sistema, procede a iniciar sesion"
+            );
+          }
+        });
+    }
+
+  }
+
+
+
